90f632ea908ca17bb0200a8dc48d1208ed42e11e
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-23 10:51:12 +0100

    Open API and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global validation
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Mangas API')
+    .setDescription('The Mangas API documentation')
+    .setVersion('1.0')
+    .addTag('mangas')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/utils/config/get-env.ts
+++ src/utils/config/get-env.ts
@@ -1,15 +1,24 @@
+const getBooleanFromString = (value: string | undefined, def = false) => {
+  if (value === undefined || value === '') return def;
+
+  return ['true', '1', 'yes', 'enabled'].includes(value.toLowerCase());
+};
+
 export const getEnv = (): EnvFile => {
   const env = process.env as unknown as RawEnvFile;
 
   return {
     apiKeys: JSON.parse(env.API_KEYS),
+    openApiDocs: getBooleanFromString(env.OPEN_API_DOCS, false),
   };
 };
 
 interface EnvFile {
   apiKeys: string[];
+  openApiDocs: boolean;
 }
 
 class RawEnvFile {
   API_KEYS: string;
+  OPEN_API_DOCS?: string;
 }
