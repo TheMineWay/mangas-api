8823c6d0ee15171bbcd8de018939976f7954e091
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-23 10:56:46 +0100

    Specify types

--- src/dtos/manga/explore/manga-explore-filters.dto.ts
+++ src/dtos/manga/explore/manga-explore-filters.dto.ts
@@ -1,6 +1,21 @@
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
 export class MangaExploreFiltersDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(64)
   name: string;
 
   // Pagination
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
   page?: number;
 }
